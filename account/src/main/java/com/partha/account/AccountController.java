1be666b054715d196469d18308d93d6cb85eea98
Partha Das <partharecrkgp@gmail.com>
2021-12-04 17:06:50 +0530

    versions

--- account/src/main/java/com/partha/account/AccountController.java
+++ account/src/main/java/com/partha/account/AccountController.java
@@ -3,6 +3,7 @@ package com.partha.account;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -20,6 +21,12 @@ public class AccountController {
 
 	@Autowired
 	RestTemplate restTemplate;
+	
+	@Value("${version}")
+	private String version;
+	
+	@Value("${payment.endpoint}")
+	private String url;
 
 	@GetMapping("getaccount")
 	public String getCustomer(@RequestHeader(value = "Authorization", required = false) String authorization) {
@@ -28,8 +35,8 @@ public class AccountController {
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
 		System.out.println("Authorization:"+authorization);
-		ResponseEntity<String> respEntity = restTemplate.exchange("http://payment-service:8080/getpayment",HttpMethod.GET, entity, String.class);
+		ResponseEntity<String> respEntity = restTemplate.exchange(url,HttpMethod.GET, entity, String.class);
 		//ResponseEntity<String> respEntity = restTemplate.exchange("http://localhost:8082/getpayment",HttpMethod.GET, entity, String.class);
-		return "--From Account--" + respEntity.getBody();
+		return "--From Account--version:" +version +" data:"+ respEntity.getBody();
 	}
 }
