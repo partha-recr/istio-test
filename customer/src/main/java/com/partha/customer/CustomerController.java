065bce63742dad2a858094567300bdce82f1593b
Partha Das <partharecrkgp@gmail.com>
2021-12-04 22:39:23 +0530

    route-test

--- customer/src/main/java/com/partha/customer/CustomerController.java
+++ customer/src/main/java/com/partha/customer/CustomerController.java
@@ -27,17 +27,32 @@ public class CustomerController {
 	
 	@Value("${account.endpoint}")
 	private String url;
+	
+	@Value("${account.endpoint.route}")
+	private String routeurl;
 
 	@GetMapping("getcustomer")
 	public String getCustomer(@RequestHeader(value = "Authorization", required = false) String authorization ,@RequestHeader(value = "end-user", required = false) String endUser) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Authorization", authorization);
+		headers.set("end-user", endUser);
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
 		System.out.println("Authorization :" + authorization);
 		ResponseEntity<String> respEntity = restTemplate.exchange(url,HttpMethod.GET, entity, String.class);
 		//ResponseEntity<String> respEntity = restTemplate.exchange("http://localhost:8081/getaccount",HttpMethod.GET, entity, String.class);
 		return "--From Customer---version:" +version +" End user:"+endUser+" data: " + respEntity.getBody();
+	}
+	
+	@GetMapping("getroute")
+	public String getroute(@RequestHeader(value = "Authorization", required = false) String authorization) {
+		System.out.println("Authorization value is : " + authorization);
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", authorization);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<String> respEntity = restTemplate.exchange(routeurl,HttpMethod.GET, entity, String.class);
+		return respEntity.getBody();
 	}
 
 }
