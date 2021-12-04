4e64d6c66592afd1615ff6e6eb0c86faeadabf38
Partha Das <partharecrkgp@gmail.com>
2021-12-04 14:44:11 +0530

    Initial commit

---
+++ customer/src/main/java/com/partha/customer/CustomerController.java
@@ -0,0 +1,36 @@
+package com.partha.customer;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/")
+public class CustomerController {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping("getcustomer")
+	public String getCustomer(@RequestHeader(value = "Authorization", required = false) String authorization) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", authorization);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		System.out.println("Authorization :" + authorization);
+		ResponseEntity<String> respEntity = restTemplate.exchange("http://account-service:8080/getaccount",HttpMethod.GET, entity, String.class);
+		//ResponseEntity<String> respEntity = restTemplate.exchange("http://localhost:8081/getaccount",HttpMethod.GET, entity, String.class);
+		return "--From Customer--" + respEntity.getBody();
+	}
+
+}
