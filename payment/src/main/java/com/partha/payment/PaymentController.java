7a6f712fad1ce2ebaef58ea372c2deb827c01a5f
Partha Das <partharecrkgp@gmail.com>
2021-12-04 17:07:20 +0530

    version

--- payment/src/main/java/com/partha/payment/PaymentController.java
+++ payment/src/main/java/com/partha/payment/PaymentController.java
@@ -1,5 +1,6 @@
 package com.partha.payment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class PaymentController {
+	
+	@Value("${version}")
+	private String version;
 
 	@GetMapping("getpayment")
 	public String getPayment(@RequestHeader(value = "Authorization", required = false) String authorization) {
 		System.out.println("Authorization value is : " + authorization);
-		return "--From Payment";
+		return "--From Payment with Version:" +version;
 	}
 }
