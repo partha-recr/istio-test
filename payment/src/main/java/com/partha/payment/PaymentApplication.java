67fa0d596ea63625d014fa129b4f8f5ee0547408
Partha Das <partharecrkgp@gmail.com>
2021-12-08 21:08:16 +0530

    prometheus with Timer

--- payment/src/main/java/com/partha/payment/PaymentApplication.java
+++ payment/src/main/java/com/partha/payment/PaymentApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @SpringBootApplication
 public class PaymentApplication {
 
@@ -15,6 +18,11 @@ public class PaymentApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 
 }
