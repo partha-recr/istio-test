8dc4ca73df8b8c0533803ad25445670bb92fbb3d
Partha Das <partharecrkgp@gmail.com>
2021-12-10 10:46:20 +0530

    telemetry

--- customer/src/main/java/com/partha/customer/GreetingController.java
+++ customer/src/main/java/com/partha/customer/GreetingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/customer/")
 public class GreetingController {
 	
 	@Autowired
